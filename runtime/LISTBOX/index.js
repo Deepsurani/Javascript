abb03ed7db241e1d4bc5f43f21d18272a2fba3cb
Deepsurani <Deepupatel112233@gmail.com>
2024-05-28 08:00:06 +0530

    second commit

--- runtime/LISTBOX/index.js
+++ runtime/LISTBOX/index.js
@@ -8,4 +8,20 @@ function btn1(){
         list1.appendChild(opt);
         console.log(list1)
 }
+var b2 = document.createElement("input");
+b2.type = "button";
+b2.name = "btn2";
+b2.id = "btn2";
+b2.value = ">";
+b2.onclick = function () {
+    var opt1 = document.createElement("option");
+    opt1.value = list1.value;
+    opt1.innerHTML = list1.value;
 
+    list2.appendChild(opt1);
+    var Drop1 = document.getElementById("list1");
+    var OpDrop1 = Drop1.options[Drop1.selectedIndex];
+    Drop1.removeChild(OpDrop1);
+    console.log(OpDrop1);
+}
+document.getElementById("box").appendChild(b2);
